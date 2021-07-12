69834b8e75e5bda084b0997102be93d619f93a50
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-12 02:15:54 -0300

    use fetch query

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,65 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { graphql } from 'babel-plugin-relay/macro'
 import {
+  fetchQuery,
   RelayEnvironmentProvider,
-  useLazyLoadQuery,
 } from 'react-relay/hooks'
 
-import { AppQuery } from './__generated__/AppQuery.graphql'
+import { AppQuery, AppQueryResponse } from './__generated__/AppQuery.graphql'
 import RelayEnvironment from './relay/RelayEnvironment'
 import './App.css';
 
 function App() {
-  const data = useLazyLoadQuery<AppQuery>(
-    graphql`
-      query AppQuery {
-        videosByTitle (title: "Ronaldinho") {
-          items { _id: id { kind, videoId }, snippet { title } }
+  const [data, setData] = useState<AppQueryResponse>()
+  const [inputValue, setInputValue] = useState('')
+
+  const fetchMoviesAndVideos = (title: string) => {
+    fetchQuery<AppQuery>(
+      RelayEnvironment,
+      graphql`
+        query AppQuery($title: String!) {
+          videosByTitle (title: $title) {
+            items { _id: id { kind, videoId }, snippet { title } }
+          }
+          moviesByTitle (title: $title) {
+            Search { Title, Year }
+          }
         }
-        moviesByTitle (title: "Ronaldinho") {
-          Search { Title, Year }
-        }
-      }
-    `,
-    {},
-    {},
-  )
+      `,
+      {title: title}
+    )
+    .toPromise()
+    .then(data => {
+      setData(data)
+    })
+    .catch(error => {
+      console.log('fetchMoviesAndVideos fetchQuery', error)
+    })
+  }
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setInputValue(event.currentTarget.value)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    fetchMoviesAndVideos(inputValue)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="search">Search videos and movies</label>
+          <input 
+            type="text" 
+            name="search" 
+            id="search" 
+            onChange={handleChange} 
+            value={inputValue} 
+          />
+          <button type="submit">Search</button>
+        </form>
         <div>
           <p>Videos list</p>
           <ul>
