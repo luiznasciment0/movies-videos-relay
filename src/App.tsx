20c5366d86b15c7748280a144d8df616f38ba6be
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-12 00:39:29 -0300

    use lazy load query

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,54 @@ import React, { Suspense } from 'react';
 import { graphql } from 'babel-plugin-relay/macro'
 import {
   RelayEnvironmentProvider,
-  loadQuery,
-  usePreloadedQuery,
-  PreloadedQuery,
+  useLazyLoadQuery,
 } from 'react-relay/hooks'
 
+import { AppQuery } from './__generated__/AppQuery.graphql'
 import RelayEnvironment from './relay/RelayEnvironment'
 import './App.css';
-import { OperationType } from 'relay-runtime';
 
-const MoviesAndVideosByTitleQuery = graphql`
-  query AppQuery {
-    videosByTitle (title: "Ronaldinho") {
-      items { _id: id { kind, videoId }, snippet { title } }
-    }
-    moviesByTitle (title: "Ronaldinho") {
-      Search { Title, Year }
-    }
+function App() {
+  const data = useLazyLoadQuery<AppQuery>(
+    graphql`
+      query AppQuery {
+        videosByTitle (title: "Ronaldinho") {
+          items { _id: id { kind, videoId }, snippet { title } }
+        }
+        moviesByTitle (title: "Ronaldinho") {
+          Search { Title, Year }
+        }
+      }
+    `,
+    {},
+    {},
+  )
+
+  if (!data || !data.videosByTitle || !data.videosByTitle.items || !data.moviesByTitle || !data.moviesByTitle.Search) {
+    return (
+      <div>Loading...</div>
+    )
   }
-`
-
-const preloadedQuery = loadQuery(RelayEnvironment, MoviesAndVideosByTitleQuery, {})
-
-function App({ preloadedQuery }: { preloadedQuery: PreloadedQuery<OperationType, {}> }) {
-  const data: any = usePreloadedQuery(MoviesAndVideosByTitleQuery, preloadedQuery)
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{data.videosByTitle.items[0].snippet.title}</p>
-        <p>{data.moviesByTitle.Search[0].Title}</p>
+        <div>
+          <p>Videos list</p>
+          <ul>
+            {data.videosByTitle.items.map((item) => (
+              <li key={item?._id?.videoId}>Title: {item?.snippet?.title}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>Movies list</p>
+          <ul>
+            {data.moviesByTitle.Search.map((item) => (
+              <li key={`key-${item?.Title}-${item?.Year}`}>Title: {item?.Title}</li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   )
@@ -41,7 +59,7 @@ function AppRoot() {
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
       <Suspense fallback={'Loading...'}>
-        <App preloadedQuery={preloadedQuery} />
+        <App />
       </Suspense>
     </RelayEnvironmentProvider>
   )
