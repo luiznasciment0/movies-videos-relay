6273bc145b74b6f8961f581e5b82f944f5288b79
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-11 23:17:58 -0300

    fix fetchgraphql function

--- src/services/fetchGraphQL.ts
+++ src/services/fetchGraphQL.ts
@@ -1,20 +1,21 @@
 const fetchGraphQL = async (text: string, variables?: any) => {
-    try {
-        const response = await fetch('https://movies-videos-graphql.herokuapp.com/graphql', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify({
-                query: text,
-            })
-        })
-    
-        return await response.json()
-    } catch (error) {
-        console.log(error)
-        throw error
-    }
+  try {
+    const response = await fetch('https://movies-videos-graphql.herokuapp.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: text,
+        variables,
+      })
+    })
+
+    return await response.json()
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
 }
 
 export default fetchGraphQL
