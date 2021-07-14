23c511a346a2b18424302c2aefa09048fecebd00
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-13 23:56:45 -0300

    fix dumb mistake

--- src/components/Keyboard/keyboardKeys.ts
+++ src/components/Keyboard/keyboardKeys.ts
@@ -8,197 +8,192 @@ export type KeyboardKeysMap = KeyboardKey[]
 
 export const keyboardKeys: KeyboardKeysMap = [
   {
-    id: 1,
+    id: 0,
     label: 'a',
     value: 'a'
   },
   {
-    id: 2,
+    id: 1,
     label: 'b',
     value: 'b'
   },
   {
-    id: 3,
+    id: 2,
     label: 'c',
     value: 'c'
   },
   {
-    id: 4,
+    id: 3,
     label: 'd',
     value: 'd'
   },
   {
-    id: 5,
+    id: 4,
     label: 'e',
     value: 'e'
   },
   {
-    id: 6,
+    id: 5,
     label: 'f',
     value: 'f'
   },
   {
-    id: 7,
+    id: 6,
     label: 'g',
     value: 'g'
   },
   {
-    id: 8,
+    id: 7,
     label: 'h',
     value: 'h'
   },
   {
-    id: 9,
+    id: 8,
     label: 'i',
     value: 'i'
   },
   {
-    id: 10,
+    id: 9,
     label: 'j',
     value: 'j'
   },
   {
-    id: 11,
+    id: 10,
     label: 'k',
     value: 'k'
   },
   {
-    id: 12,
+    id: 11,
     label: 'l',
     value: 'l'
   },
   {
-    id: 13,
-    label: 'i',
-    value: 'i'
-  },
-  {
-    id: 14,
+    id: 12,
     label: 'm',
     value: 'm'
   },
   {
-    id: 15,
+    id: 13,
     label: 'n',
     value: 'n'
   },
   {
-    id: 16,
+    id: 14,
     label: 'o',
     value: 'o'
   },
   {
-    id: 17,
+    id: 15,
     label: 'p',
     value: 'p'
   },
   {
-    id: 18,
+    id: 16,
     label: 'q',
     value: 'q'
   },
   {
-    id: 19,
+    id: 17,
     label: 'r',
     value: 'r'
   },
   {
-    id: 20,
+    id: 18,
     label: 's',
     value: 's'
   },
   {
-    id: 21,
+    id: 19,
     label: 't',
     value: 't'
   },
   {
-    id: 22,
+    id: 20,
     label: 'u',
     value: 'u'
   },
   {
-    id: 23,
+    id: 21,
     label: 'v',
     value: 'v'
   },
   {
-    id: 24,
+    id: 22,
     label: 'w',
     value: 'w'
   },
   {
-    id: 25,
+    id: 23,
     label: 'x',
     value: 'x'
   },
   {
-    id: 26,
+    id: 24,
     label: 'y',
     value: 'y'
   },
   {
-    id: 27,
+    id: 25,
     label: 'z',
     value: 'z'
   },
   {
-    id: 28,
+    id: 26,
     label: '0',
     value: '0'
   },
   {
-    id: 29,
+    id: 27,
     label: '1',
     value: '1'
   },
   {
-    id: 30,
+    id: 28,
     label: '2',
     value: '2'
   },
   {
-    id: 31,
+    id: 29,
     label: '3',
     value: '3'
   },
   {
-    id: 32,
+    id: 30,
     label: '4',
     value: '4'
   },
   {
-    id: 33,
+    id: 31,
     label: '5',
     value: '5'
   },
   {
-    id: 34,
+    id: 32,
     label: '6',
     value: '6'
   },
   {
-    id: 35,
+    id: 33,
     label: '7',
     value: '7'
   },
   {
-    id: 36,
+    id: 34,
     label: '8',
     value: '8'
   },
   {
-    id: 37,
+    id: 35,
     label: '9',
     value: '9'
   },
   {
-    id: 38,
+    id: 36,
     label: 'space',
     value: ' '
   },
   {
-    id: 39,
+    id: 37,
     label: 'backspace',
     value: 'backspace'
   }
