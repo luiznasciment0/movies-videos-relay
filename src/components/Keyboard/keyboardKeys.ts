0c25e7fa89c572a4a26e729e9a17aed52dfa0294
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-13 23:42:47 -0300

    create keyboardkeys types and map

---
+++ src/components/Keyboard/keyboardKeys.ts
@@ -0,0 +1,205 @@
+export type KeyboardKey = {
+  id: number
+  label: string
+  value: string
+}
+
+export type KeyboardKeysMap = KeyboardKey[]
+
+export const keyboardKeys: KeyboardKeysMap = [
+  {
+    id: 1,
+    label: 'a',
+    value: 'a'
+  },
+  {
+    id: 2,
+    label: 'b',
+    value: 'b'
+  },
+  {
+    id: 3,
+    label: 'c',
+    value: 'c'
+  },
+  {
+    id: 4,
+    label: 'd',
+    value: 'd'
+  },
+  {
+    id: 5,
+    label: 'e',
+    value: 'e'
+  },
+  {
+    id: 6,
+    label: 'f',
+    value: 'f'
+  },
+  {
+    id: 7,
+    label: 'g',
+    value: 'g'
+  },
+  {
+    id: 8,
+    label: 'h',
+    value: 'h'
+  },
+  {
+    id: 9,
+    label: 'i',
+    value: 'i'
+  },
+  {
+    id: 10,
+    label: 'j',
+    value: 'j'
+  },
+  {
+    id: 11,
+    label: 'k',
+    value: 'k'
+  },
+  {
+    id: 12,
+    label: 'l',
+    value: 'l'
+  },
+  {
+    id: 13,
+    label: 'i',
+    value: 'i'
+  },
+  {
+    id: 14,
+    label: 'm',
+    value: 'm'
+  },
+  {
+    id: 15,
+    label: 'n',
+    value: 'n'
+  },
+  {
+    id: 16,
+    label: 'o',
+    value: 'o'
+  },
+  {
+    id: 17,
+    label: 'p',
+    value: 'p'
+  },
+  {
+    id: 18,
+    label: 'q',
+    value: 'q'
+  },
+  {
+    id: 19,
+    label: 'r',
+    value: 'r'
+  },
+  {
+    id: 20,
+    label: 's',
+    value: 's'
+  },
+  {
+    id: 21,
+    label: 't',
+    value: 't'
+  },
+  {
+    id: 22,
+    label: 'u',
+    value: 'u'
+  },
+  {
+    id: 23,
+    label: 'v',
+    value: 'v'
+  },
+  {
+    id: 24,
+    label: 'w',
+    value: 'w'
+  },
+  {
+    id: 25,
+    label: 'x',
+    value: 'x'
+  },
+  {
+    id: 26,
+    label: 'y',
+    value: 'y'
+  },
+  {
+    id: 27,
+    label: 'z',
+    value: 'z'
+  },
+  {
+    id: 28,
+    label: '0',
+    value: '0'
+  },
+  {
+    id: 29,
+    label: '1',
+    value: '1'
+  },
+  {
+    id: 30,
+    label: '2',
+    value: '2'
+  },
+  {
+    id: 31,
+    label: '3',
+    value: '3'
+  },
+  {
+    id: 32,
+    label: '4',
+    value: '4'
+  },
+  {
+    id: 33,
+    label: '5',
+    value: '5'
+  },
+  {
+    id: 34,
+    label: '6',
+    value: '6'
+  },
+  {
+    id: 35,
+    label: '7',
+    value: '7'
+  },
+  {
+    id: 36,
+    label: '8',
+    value: '8'
+  },
+  {
+    id: 37,
+    label: '9',
+    value: '9'
+  },
+  {
+    id: 38,
+    label: 'space',
+    value: ' '
+  },
+  {
+    id: 39,
+    label: 'backspace',
+    value: 'backspace'
+  }
+]
