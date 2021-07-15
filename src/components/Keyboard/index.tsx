4a432a12552134d5fb8500e1b0138824b95886c3
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-14 23:44:57 -0300

    fix navigation

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -1,52 +1,60 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import KeyboardKey from './Key'
 
 import { keyboardKeys } from './keyboardKeys'
 import { Keyboard as StyledKeyboard } from './styles'
 import useKeyPress from './useKeyPress'
 
-const arrayRemainder = (arrayLength: number, cursorPosition: number) =>
-  arrayLength - cursorPosition
-
-const arrayPast = (cursorPosition: number) => cursorPosition - 1
+const rowLength = 6
 
 const Keyboard = () => {
   const [inputValue, setInputValue] = useState('')
   const [selected, setSelected] = useState()
   const [cursor, setCursor] = useState(0)
 
-  const arrowDownPress = useKeyPress('ArrowDown')
-  const arrowUpPress = useKeyPress('ArrowUp')
-  const arrowLeftPress = useKeyPress('ArrowLeft')
-  const arrowRightPress = useKeyPress('ArrowRight')
-  const enterKeyPress = useKeyPress('Enter')
+  const currentItem = keyboardKeys[cursor]
+  const rightItemsLength = rowLength - currentItem.positionInRow
+  const leftItemsLength = currentItem.positionInRow - 1
 
-  useEffect(() => {
-    if (arrowDownPress) {
+  useKeyPress({
+    key: 'ArrowDown',
+    action: () => {
       setCursor((prevState) => {
-        const totalGridItems = 37
-        const rowLength = 6
-        const cursorPositionInRow =
-          cursor > 5 ? cursor + 1 / rowLength : cursor + 1
+        const nextPosition = prevState + rightItemsLength + leftItemsLength + 1
+        const exceededArray = nextPosition > keyboardKeys.length
 
-        const remainderSpace = arrayRemainder(rowLength, cursorPositionInRow)
-        const leftSpace = arrayPast(cursorPositionInRow)
-        const nextPosition = prevState + remainderSpace + leftSpace + 1
-
-        return nextPosition > totalGridItems ? totalGridItems : nextPosition
+        return exceededArray ? keyboardKeys.length : nextPosition
       })
     }
+  })
 
-    if (arrowRightPress) {
+  useKeyPress({
+    key: 'ArrowUp',
+    action: () => {
+      setCursor((prevState) => {
+        const nextPosition = prevState - rightItemsLength - leftItemsLength - 1
+        const exceededArray = nextPosition < 1
+
+        return exceededArray ? 0 : nextPosition
+      })
+    }
+  })
+
+  useKeyPress({
+    key: 'ArrowRight',
+    action: () => {
       setCursor((prevState) =>
         prevState < keyboardKeys.length - 1 ? prevState + 1 : prevState
       )
     }
+  })
 
-    if (arrowLeftPress) {
+  useKeyPress({
+    key: 'ArrowLeft',
+    action: () => {
       setCursor((prevState) => (prevState > 0 ? prevState - 1 : prevState))
     }
-  }, [arrowDownPress, arrowRightPress, arrowLeftPress, cursor])
+  })
 
   return (
     <div>
@@ -54,7 +62,7 @@ const Keyboard = () => {
       <StyledKeyboard>
         {keyboardKeys.map((item, index) => (
           <KeyboardKey
-            key={item.id}
+            key={index}
             active={index === cursor}
             item={item}
             setSelected={setSelected}
